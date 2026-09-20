a3545f93a192c8763ea27c2507924e7511d74326
MinT <tnm1042002@gmail.com>
2026-09-20 10:03:09 +0700

    Update repository.py

--- src/fastapi_rebuild/features/users/repository.py
+++ src/fastapi_rebuild/features/users/repository.py
@@ -17,4 +17,4 @@ class UserRepository:
         self.session.add(user)
 
     async def get(self, user_id: int) -> User | None:
-    return await self.session.get(User, user_id)
+        return await self.session.get(User, user_id)
